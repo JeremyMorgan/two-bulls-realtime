2618832b3d8289268e036370f2ed210cc8ec0ec0
Jeremy Morgan <jeremymorgan@gmail.com>
2014-06-23 16:07:00 -0700

    Added links, changes

--- js/main.js
+++ js/main.js
@@ -12,16 +12,10 @@ $('#odf').feeds({
 });
 $('#googlenews').feeds({
     feeds: {
-        feed1: 'https://news.google.com/news/feeds?gl=us&hl=en&ie=UTF-8&output=rss&q=Two%20Bulls%20Wildfire%20Bend&um=1',    
+        feed1: 'https://news.google.com/news/feeds?gl=us&hl=en&ie=UTF-8&output=rss&q=Owens%20Wildfire%20Hood%20River&um=1',    
 
     },
     max: 10
 });
 
-$('#centralorfireinfo').feeds({
-    feeds: {
-        feed1: 'http://centralorfireinfo.blogspot.com/feeds/posts/default',
-    },
-    max: 10
-});
 
